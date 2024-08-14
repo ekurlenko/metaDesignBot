e8b587d3d1cf5a9eeb0df2d2d96d5949ae3e9067
Egor <ekurlenko17@gmail.com>
2024-08-14 14:53:29 +0300

    text changed

--- keyboards/phoneKeyboard.py
+++ keyboards/phoneKeyboard.py
@@ -4,6 +4,6 @@ from aiogram.types import ReplyKeyboardMarkup
 
 def get_phone_keyboard() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardBuilder()
-    keyboard.button(text="Отправить свой номер телефона", request_contact=True)
+    keyboard.button(text="Отправить свой номер телефона и вы увидите цену ремонта", request_contact=True)
     return keyboard.as_markup(resize_keyboard=True,
                               one_time_keyboard=True)
