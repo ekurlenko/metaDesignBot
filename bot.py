fd4255f92f879747f6249084ad7f48acc7be3bdb
Egor Kurlenko <ekurlenko17@gmail.com>
2025-03-13 14:36:27 +0300

    scheduler job

--- bot.py
+++ bot.py
@@ -5,7 +5,11 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.redis import RedisStorage
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 from handlers import orderConstructor, start, repairCategories, moreInfo, designProject, aboutUs, portfolio
+
+from misc.utils import pull_orders
 
 from dotenv import load_dotenv
 
@@ -23,6 +27,11 @@ async def main(bot: Bot):
     dp = Dispatcher(storage=RedisStorage.from_url(os.getenv('REDIS_URL')))
     dp.include_routers(start.router, orderConstructor.router, repairCategories.router,
                        moreInfo.router, designProject.router, aboutUs.router, portfolio.router)
+
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(pull_orders, 'interval', minutes=30, args=[bot, ])
+    scheduler.start()
+
     await dp.start_polling(bot)
 
 
