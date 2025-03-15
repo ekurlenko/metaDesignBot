e6814a34b473342e721c0cde68425158f9520e01
Egor Kurlenko <ekurlenko17@gmail.com>
2025-03-15 17:14:32 +0300

    PooledMySQL

--- dataBase/config.py
+++ dataBase/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db = peewee_async.MySQLDatabase(
+db = peewee_async.PooledMySQLDatabase(
     database=os.getenv('DB_NAME'),
     user=os.getenv('DB_USER'),
     host=os.getenv('DB_HOST'),
