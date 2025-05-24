8aab7182a8dbf82c104a40e71c4fa520f694898d
Egor Kurlenko <ekurlenko17@gmail.com>
2025-05-24 14:58:58 +0300

    pool size

--- dataBase/config.py
+++ dataBase/config.py
@@ -8,11 +8,11 @@ db = peewee_async.PooledMySQLDatabase(
     database=os.getenv('DB_NAME'),
     user=os.getenv('DB_USER'),
     host=os.getenv('DB_HOST'),
-    connect_timeout = 120,
     pool_params={
         "minsize": 2,
         "maxsize": 10,
-        "pool_recycle": 5
+        "pool_recycle": 55,
+        "stale_timeout": 60,
     },
     password=os.getenv('DB_PASSWORD', default='')
 )
