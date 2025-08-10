088aac59a612439a3646fc5999f200d4c36cf603
bublik911 <ekurlenko17@gmail.com>
2025-08-10 23:38:42 +0300

    repo

--- dataBase/repositories/order.py
+++ dataBase/repositories/order.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 
 from dataBase.models.OrderModel import OrderModel
 from dataBase.repositories.base import BaseRepository
@@ -12,3 +13,11 @@ class OrderRepository(BaseRepository):
                 done_at=datetime.now()
             )
             return order
+
+    def get_not_done(self) -> List[OrderModel]:
+        result = OrderModel.select().where(OrderModel.done_at == None)
+        return result
+
+    def update_done_at(self, order: OrderModel) -> None:
+        order.done_at = datetime.now()
+        order.save()
